94bf2e9d48f028be119461f60f7bea49e7ef33a1
tiwaryramesh95@gmail.com <tiwary705014@gmail.com>
2025-04-29 15:07:25 -0300

    update l11.p1.js and add new file

--- l11.p1.js
+++ l11.p1.js
@@ -187,14 +187,31 @@
 h1 = document.querySelector("h1");
 
 function changeColor(color, delay) {
-    new Promise((resolve,reject) => {
+     return new Promise((resolve,reject) => {
         setTimeout(() => {
             h1.style.color = color;
             resolve("color changed!");
                },delay);
         });
     }
-
+    
+    changeColor("red", 1000)
+    .then(() => {
+        console.log ("red color was completed");
+        return changeColor("orange",1000);
+    })
+    .then(() =>{
+        console.log("orange color was completed ");
+        return changeColor("green",1000);
+    })
+    .then(() =>{
+        console.log("green color was completed ");
+        return changeColor("yellow",1000);
+    })
+    .then(() =>{
+        console.log("yellow color was completed ");
+    
+    })
 // changeColor("red", 1000, () => {
 //     changeColor("orange", 1000, () => {
 //         changeColor("green",1000, () => {
